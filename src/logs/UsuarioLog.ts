2c054f8439c3487369cbabe1a0a56b1798f57951
TiItoVG <vgailton20@gmail.com>
2025-02-17 20:30:29 -0400

    UsuarioLog.ts

--- src/logs/UsuarioLog.ts
+++ src/logs/UsuarioLog.ts
@@ -4,6 +4,6 @@ import sqlite3 from 'sqlite3';
 export class UsuarioLog {
   async log(action: string) {
     const db = await open({ filename: './data/logs.db', driver: sqlite3.Database });
-    await db.run('INSERT INTO logs (descricao, data) VALUES (?, ?)', [action, new Date().toISOString()]);
+    await db.run('INSERT INTO logs (descricao, data) VALUES (?, ?)', [action, new Date().toUTCString()]);
   }
 }

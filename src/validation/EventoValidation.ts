b3d715f47dc9de605b8d087511a27319777c772d
TiItoVG <vgailton20@gmail.com>
2025-02-18 18:17:05 -0400

    Commit1

--- src/validation/EventoValidation.ts
+++ src/validation/EventoValidation.ts
@@ -2,6 +2,6 @@ import { z } from 'zod';
 
 export const eventoSchema = z.object({
   nome: z.string().min(3),
-  data: z.string().regex(/^\\d{4}-\\d{2}-\\d{2}$/),
+  data: z.string().regex(/^\d{2}-\d{2}-\d{4}$/, "Formato inválido. Use DD-MM-AAAA"),
   usuario_id: z.number().int(),
 });
